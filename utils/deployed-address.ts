d00586d5394b4fe08196800d1bf606cb9cb753ef
JU4RA <donnysuhend@gmail.com>
2024-07-30 11:09:09 +0700

    Update deployed-address.ts

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x2E87d20ddfa1F8Cfc797b178b9F37e05c2d78167'
+export const ProxyAdmin = '0x5C3106FAC6A7E2c916e94AB00c28755d40d7570F'
+export const SWTRImplementation = '0xE35Fd53Fa8040125369E3C3a438A9421b9107dF4'
     
